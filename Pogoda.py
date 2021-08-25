6b1feb4d2c9c9462b3399f6fe6ebb679bf112d5b
ZloiDyadka <daniil9797979797@gmail.com>
2021-08-26 02:36:29 +0300

    pogoda

--- Pogoda.py
+++ Pogoda.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fileencoding=utf-8
-
+import telebot
 import requests
+import pandas as pd
 #from requests.models import Response
 
-city_name = 'Нарьян-Мар'
+#city_name = 'Нарьян-Мар'
+token='1951761639:AAE-ewTNw8w6uyaHY4UP0wVx7hxjC0kbelk'
+bot = telebot.TeleBot(token)
 
 def pogoda(city_name):
     api_key = 'f58506a15948f751314a3023408b4aa4'
@@ -13,8 +16,14 @@ def pogoda(city_name):
     response = requests.get(ap)
     data = response.json()
     city = data['name']
-    temp = str(round(data['main']['temp']-273.15, 2))+'℃'
+    temp = str(round(data['main']['temp']-273.15, 1))+'℃'
     weather = str(data['wind']['speed'])+' м/с'
     humidity = str(data['main']['humidity'])+'%'
-    return city, temp, weather, humidity
-print(pogoda(city_name))
+    s= [city, temp, weather, humidity]
+    return s
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    bot.send_message(message.from_user.id, str(pd.DataFrame(pogoda(message.text), index=['Город', 'Температура', 'Скорость ветра', 'Влажность'])))
+bot.polling(none_stop=True, interval=0)
